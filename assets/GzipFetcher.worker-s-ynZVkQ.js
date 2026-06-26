12b30f6f5ebcca0aa1facf9a1e98b65f1cc36e2f
Billy Charlton <mail@billyc.cc>
2026-06-26 09:52:56 +0200

    gh-pages

---
+++ assets/GzipFetcher.worker-s-ynZVkQ.js
@@ -0,0 +1,1 @@
+function e(r){return new Worker("/assets/GzipFetcher.worker-Bzu-hr33.js",{name:r?.name})}export{e as W};
---
+++ assets/NewXmlFetcher.worker-_EkVgoO_.js
@@ -0,0 +1,1 @@
+function r(e){return new Worker("/assets/NewXmlFetcher.worker-Yt4cN-oJ.js",{name:e?.name})}export{r as W};
